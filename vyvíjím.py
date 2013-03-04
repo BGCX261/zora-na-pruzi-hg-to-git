ac9ba35d76123469e6caf0401650ee55f87a1850
Петр Болф <petr.bolf@domogled.eu>
2013-03-04 18:08:42 +0100

    Nic, jen hraní

--- vyvíjím.py
+++ vyvíjím.py
@@ -212,64 +212,34 @@ def   zkúšám():
                 print(vlastnost)
     
 def graf():
-    from zora_na_pruzi.strojmir.xml.graphml.Graf import Graf
+    
     from zora_na_pruzi.strojmir.xml.graphml import načtu_graf
     import lxml.etree
     
-    graphml_soubor = './zora_na_pruzi/strojmir/xml/graphml/testuji_vzorový_graf.graphml'
+    graphml_soubor = './stroj/data/skupiny.graphml'
     cesta_k_graphml_souboru = os.path.join(os.path.dirname(__file__),  graphml_soubor)
     
    
-    
     print('Testuji na testovacím grafu {}'.format(cesta_k_graphml_souboru | F.SOUBOR) | F.TEST.START)
     
-#    graf = Graf(cesta_k_graphml_souboru)
-#    tree = graf.xml
     tree = načtu_graf(cesta_k_graphml_souboru)
-    assert isinstance(tree,  lxml.etree._ElementTree)
     
     root = tree.getroot()
     
-    assert isinstance(root,  lxml.etree.ElementBase) 
+#    from zora_na_pruzi.strojmir.xml.graphml import graphml_elementy as gml
     
-    uzly = list(root.uzly)
-    assert len(uzly) == 14
+    def piš(element,  level = 0):
+        tag = element.__class__.__name__
+        if tag in ('GRAPHML', 'GRAPH',  'NODE',  'EDGE'):
+            print('\t'*level,  tag)
+        elif tag in ('_Element', ):
+            print('\t'*level,  element.tag)
+        else:
+            print('\t'*level,  tag)
+        for potomek in element:
+            piš(potomek,  level + 1)
     
-    vazby = list(root.vazby)
-    assert len(vazby) == 12
-    
-    uzel = uzly[0]
-    
-    data = uzel.data
-    assert len(data) == 1
-    
-    údaj = data[0]
-    
-    assert údaj.jméno == 'jméno'
-    print(údaj.jméno)
-    print(údaj)
-    assert údaj.datový_typ == 'string'
-    assert údaj.default is None
-    assert údaj.klíč == root.klíče['d2']
-    assert id(údaj.klíč) == id(root.klíče['d2'])
-    
-#    druhý graf 
-    graphml_soubor = './zora_na_pruzi/strojmir/xml/graphml/testuji_vzorový_graf_2.graphml'
-    cesta_k_graphml_souboru = os.path.join(os.path.dirname(__file__),  graphml_soubor)
-    print(cesta_k_graphml_souboru)
-#    graf2 = Graf(cesta_k_graphml_souboru)
-#    tree2 = graf2.xml
-    tree2 = načtu_graf(cesta_k_graphml_souboru)
-    root2 = tree2.getroot()
-    uzly = list(root2.uzly)
-    uzel = uzly[0]
-    print(uzel.jméno)
-    data = uzel.data
-    
-    údaj = data[0]
-    print(údaj.jméno)
-    print(údaj)
-    assert údaj.jméno == 'jiné jméno'   
+    piš(root)
   
 
 if __name__ == '__main__':
