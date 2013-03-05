9ddf0b64ea3261700a2e1e6a95cbce43afd55c0e
Петр Болф <petr.bolf@domogled.eu>
2013-03-05 11:03:34 +0100

    Udělátka

---
+++ chmod.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+chmod ug+x $@
---
+++ hg_ignore.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# Copyright (c) 2012 Домоглед  <domogled@domogled.eu>
+# @author Петр Болф <petr.bolf@domogled.eu>
+
+'''
+Hen je program, který přidá soubor do hgignore
+'''
+
+__version__ = '0.0.1'
+__author__ = 'Петр Болф <petr.bolf@domogled.eu>'
+
+
+import os
+
+def čtu_hg_ignore():
+    '''
+    spouštím funkci main()
+    '''
+    
+    import glob
+    
+    with open('.hgignore',  mode='r',  encoding = 'UTF-8') as soubor:
+        for ignore in soubor:
+            print(ignore)
+            if ignore.startswith('glob'):
+                glob_cesta = ignore.split(':')[1]
+                glob_cesta = glob_cesta.strip()
+            else:
+                glob_cesta = ignore
+            
+            print('HLEDÁM {}'.format(glob_cesta))
+                
+            for soubor in glob.glob(glob_cesta):
+                print(soubor)
+
+def do_ignore(soubor):
+    if not os.path.isfile(soubor) and not os.path.isdir(soubor):
+        raise ValueError('Soubor {} nejestvuje'.format(soubor))
+        
+    soubor = os.path.relpath(os.path.abspath(soubor),  os.path.dirname(__file__))
+    
+    if soubor.startswith('..'):
+        raise ValueError('Soubor {} není v adresáři projektu.'.format(soubor))
+        
+    
+    with open('.hgignore',  mode='a',  encoding = 'UTF-8') as hgignore:
+        hgignore.write(soubor)
+        
+    print('Soubor {} úspěšně přidán do .hgignore.'.format(soubor))
+
+if __name__ == '__main__':
+
+    print(__doc__)
+
+    import argparse
+    #  nejdříve si parser vytvořím
+    parser = argparse.ArgumentParser()
+
+#   a pak mu nastavím jaké příkazy a parametry má přijímat
+    parser.add_argument('--version', '-v',  action='version', version='%(prog)s, искушител {}'.format(__version__))
+    
+    parser.add_argument('soubor')
+    
+    #    a včíl to možu rozparsovat
+    args = parser.parse_args()
+    
+    print('soubor {} přidám do .hgignore'.format(args.soubor))
+
+#    čtu_hg_ignore()
+    do_ignore(args.soubor)
