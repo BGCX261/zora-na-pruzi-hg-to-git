51307916da691000efa6b7f352e57c3d84447c7f
Петр Болф <petr.bolf@domogled.eu>
2013-03-13 21:08:59 +0100

    Přejmenování zpět na velká písmena

---
+++ zora_na_pruzi/strojmir/xml/svg/TITLE.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Copyright (c) 2012 Домоглед  <domogled@domogled.eu>
+# @author Петр Болф <petr.bolf@domogled.eu>
+
+'''
+Hen je třída, která ...
+'''
+#import lxml.etree
+from . import __ELEMENT_SVG,  NAMESPACE
+
+
+class title(__ELEMENT_SVG):
+#    pass
+    
+    TAG = '{{{}}}title'.format(NAMESPACE)
+    
+ 
