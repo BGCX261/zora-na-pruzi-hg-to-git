0c2206f10c4f525e6eb58561030a1a8d0052be57
Петр Болф <petr.bolf@domogled.eu>
2013-05-28 00:34:54 +0200

    neo4j startuje server

--- neo4j.py
+++ neo4j.py
@@ -9,16 +9,43 @@ Hen je program, který spustí neo4j server
 __version__ = '0.0.1'
 __author__ = 'Петр Болф <petr.bolf@domogled.eu>'
 
-from py2neo import neo4j
+import os
+import py2neo
+#from py2neo import neo4j
 
+NEO4J_URL = 'http://localhost:7474/db/data/'
+NEO4J_ADRESÁŘ = '/opt/neo4j/databáze/testovací'
+NEO4J_BIN = 'bin/neo4j'
+NEO4J_SERVER_PROPERTIES = 'conf/neo4j-server.properties'
 
-def main():
+def připojím_se_k_databázi(NEO4J_URL,  NEO4J_ADRESÁŘ):
     '''
-    spouštím funkci main()
+    spouštím funkci připojím_se_k_databázi()
     '''
-    graph_db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
-    print(graph_db)
-    print(vars(graph_db))
+    try:
+        graph_db = py2neo.neo4j.GraphDatabaseService(NEO4J_URL)
+        print('připojil jsem se k neo4j databázi na {}'.format(NEO4J_URL))
+    except py2neo.rest.SocketError as e:
+        if NEO4J_ADRESÁŘ is not None:
+            příkaz = '{} start'.format(os.path.join(NEO4J_ADRESÁŘ,  NEO4J_BIN))
+            from zora_na_pruzi.system.spustím_příkaz import spustím_příkaz_a_vypíšu
+            spustím_příkaz_a_vypíšu(příkaz)
+            NEO4J_URL = zjistím_neo4j_url_z_konfigurace(NEO4J_ADRESÁŘ)
+            připojím_se_k_databázi(NEO4J_URL,  None)
+        else:
+            raise e
+            
+def zjistím_neo4j_url_z_konfigurace(NEO4J_ADRESÁŘ):
+        soubor_nastavení = os.path.join(NEO4J_ADRESÁŘ,  NEO4J_SERVER_PROPERTIES)
+        import pyproperties
+        nastavení = pyproperties.Properties(soubor_nastavení)
+#        for klíč,  hodnota in nastavení.properties.items():
+#            print(klíč,  hodnota)
+
+        nastavení.properties.setdefault('org.neo4j.server.webserver.address',  'localhost')
+        neo4j_url_webserveru = 'http://{0[org.neo4j.server.webserver.address]}:{0[org.neo4j.server.webserver.port]}{0[org.neo4j.server.webadmin.data.uri]}'.format(nastavení.properties)
+        
+        return neo4j_url_webserveru
 
 if __name__ == '__main__':
 
@@ -38,4 +65,4 @@ if __name__ == '__main__':
 #    
 #    print('soubor',  args.soubor)
 
-    main()
+    připojím_se_k_databázi(NEO4J_URL,  NEO4J_ADRESÁŘ)
