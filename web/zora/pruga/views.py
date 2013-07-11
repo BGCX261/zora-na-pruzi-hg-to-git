5272c24376d9f202a9785081f9893cd1666c14aa
Петр Болф <petr.bolf@domogled.eu>
2013-07-11 22:30:22 +0200

    Funkční příklad na view v apliakci

--- web/zora/pruga/views.py
+++ web/zora/pruga/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from datetime import datetime
+from django.http import HttpResponse
+
+def current_time(request):
+    return HttpResponse(datetime.now().strftime('%H:%M:%S'))
