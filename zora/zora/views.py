d1d7c338e790c63a030cde55de1dfae97091e0b7
Петр Болф <petr.bolf@domogled.eu>
2013-08-07 20:23:05 +0200

    statické soubory webu a spouštění serveru

--- zora/zora/views.py
+++ zora/zora/views.py
@@ -4,3 +4,7 @@ from pyramid.view import view_config
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
     return {'project': 'zora'}
+
+@view_config(route_name='admin', renderer='admin.mako')
+def admin_view(request):
+     return {'project': 'zora v'}
